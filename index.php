072c2a61af25c0a77459518aaeffdb1f34ed69ee
429273-21 <429273-21>
2019-12-05 12:17:30 +0300

    Second commit

--- index.php
+++ index.php
@@ -1,21 +1,46 @@
 <?php
-class A{
-
+class VException extends RuntimeException {
 }
-class B extends A{
-	protected a;
-	function __construct($a){
-		$this->a = $a;
+class LinearEquation {
+	protected $a;
+	protected $b;
+	protected $x;
+	function ur($a, $b){
+		if ($a != 0) {
+			$x = -1*$b/$a;
+			$this->x = $x;
+			return $x;
+		}
+		throw new VException ("Нет решения");	
 	}
 }
-class C extends B{
-	protected b;
-	function __construct($b,$a){
-	parent::__construct($a);
-		$this->b = $b,$a;
-		
+class QuadraticEquation extends LinearEquation{
+	protected $c;
+	protected $x2;
+	protected function dis($a, $b, $c) {
+		$dis = $b*$b - 4*$a*$c;
+		return $dis;
+	}
+	function ur2($a, $b, $c) {
+		$dis = $this->dis($a, $b, $c);
+		if ($a == 0){
+		$this ->ur($a , $b);
+		}
+		if ($dis > 0) {
+			$x = (-1*$b + sqrt($dis))/(2*$a);
+			$x2 = (-1*$b - sqrt($dis))/(2*$a);
+			$this->x = $x;
+			$this->x2 = $x2;
+			return array($x, $x2);
+		} elseif ($dis = 0) {
+			$x = (-1*$b)/(2*$a);
+			$this->x = $x;
+			return array($x);
+		}
+		throw new VException ("Нет решения");
 	}
 }
+?>
 
-$a = new B(new A());
-$c=new B(new C(new B(New B(New C(New A(),$a),$a))));
+
+
