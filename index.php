665ba719468909f3e8081362e9033cece6a23a75
429273-21 <429273-21>
2019-12-06 15:47:01 +0300

    лаба4

--- index.php
+++ index.php
@@ -7,23 +7,11 @@ include_once("DaniilTFG/DaniilTFGException.php");
 include_once("DaniilTFG/LinearEquation.php");
 include_once("DaniilTFG/QuadraticEquation.php");
 include_once("DaniilTFG/MyLog.php");
-$co_arr = [];
-foreach(["a", "b", "c"] as $co) {
-	$line = readline("Enter ".$co.": ");
-	$co_arr[$co] = $line === "" ? 0 : $line;
-}
-$a = $co_arr["a"];
-$b = $co_arr["b"];
-$c = $co_arr["c"];
-//DaniilTFG\Log::log("Entered numbers: " . implode(", ", $co_arr));
-DaniilTFG\Log::log("Equation: $a*x^2 + $b*x + $c = 0");
-try {
-	$solver = new DaniilTFG\QuadraticEquation($a, $b, $c);
-	
-	DaniilTFG\Log::log("Roots: " . implode(", ", $solver->solve($a, $b, $c)));
-	
-}catch(DaniilTFG\DaniilTFGException $ms) {
-	DaniilTFG\Log::log($ms->getMessage());
-} 
+
+	DaniilTFG\Log::log("Roots: " . implode(грн8грн8рн);
+	DaniilTFG\Log::log("Roots: " . implode(грн8грн8рн);
+	DaniilTFG\Log::log("Roots: " . implode(грн8грн8рн);
+	DaniilTFG\Log::log("Roots: " . implode(грн8грн8рн);
+
 DaniilTFG\Log::write();
 ?>
